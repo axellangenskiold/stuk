57b3b414873043131d0bdfba19b9d9372d1acb7f
axel.langenskiold.2531 <axel@langenskiold.se>
2024-09-12 18:59:42 +0200

    hjh

---
+++ stuk/CalenderCard.swift
@@ -0,0 +1,67 @@
+//
+//  CalenderCard.swift
+//  stuk
+//
+//  Created by Axel Langenskiöld on 2024-09-12.
+//
+
+import SwiftUI
+
+struct CalenderCard: View {
+    @State var image: String
+    @State var event: String
+    @State var nation: String
+    
+    @State var dates: [String] = [
+        "12 SEP 09.00 - 12 SEP 19.00",
+        "05 OCT 14.30 - 06 OCT 01.00",
+        "23 NOV 16.00 - 24 NOV 02.00",
+        "17 JUL 18.15 - 18 JUL 04.00",
+        "29 AUG 08.00 - 29 AUG 17.00",
+        "02 DEC 20.00 - 03 DEC 06.00",
+        "08 JUN 13.45 - 09 JUN 00.30",
+        "14 FEB 07.00 - 14 FEB 15.00",
+        "31 MAY 19.30 - 01 JUN 05.00",
+        "18 OCT 11.00 - 18 OCT 20.00",
+        "27 JAN 22.00 - 28 JAN 07.00",
+        "10 MAR 12.30 - 10 MAR 22.00",
+        "22 APR 15.00 - 22 APR 23.00",
+        "19 AUG 10.45 - 19 AUG 18.00",
+        "06 NOV 21.00 - 07 NOV 06.00",
+        "03 DEC 06.15 - 03 DEC 14.45"
+        ]
+    
+    
+    var body: some View {
+        ZStack {
+            RoundedRectangle(cornerRadius: 10)
+                .foregroundStyle(.white)
+                .frame(width: 360, height: 300)
+                .overlay() {
+                    VStack {
+                        Image(image)
+                            .resizable()
+                            .scaledToFill()
+                            .frame(width: 180, height: 100)
+                            .clipped()
+                            .clipShape(RoundedCorner(radius: 10, corners: [.topLeft, .topRight]))
+                        
+                        Text(dates.randomElement()!)
+                            .foregroundStyle(.gray)
+                            .font(.custom("Arial", size: 10))
+                        
+                        Text(event)
+                            .font(.custom("Arial", size: 14))
+                        
+                        Text(nation)
+                            .foregroundStyle(.gray)
+                            .font(.custom("Arial", size: 10))
+                    }
+                }
+        }
+    }
+}
+
+#Preview {
+    CalenderCard(image: "jotex", event: "Malmö nations körkort", nation: "Malmö Nation")
+}
--- stuk/CalenderView.swift
+++ stuk/CalenderView.swift
@@ -10,8 +10,181 @@ import SwiftUI
 struct CalenderView: View {
     @Binding var path: NavigationPath
     
+    @State var isHome: Bool = false
+    @State var isCalender: Bool = true
+    @State var isCard: Bool = false
+    @State var isTickets: Bool = false
+    @State var isProfile: Bool = false
+    @State var isRabatt: Bool = true
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            Rectangle()
+                .frame(width: .infinity, height: .infinity)
+                .foregroundStyle(.white)
+            VStack {
+                HStack {
+                    ZStack {
+                        RoundedRectangle(cornerRadius: 15)
+                            .foregroundStyle(.gray)
+                            .frame(width: 100, height: 30)
+                            .padding(.leading, 15)
+                        
+                        
+                        Text("Lund")
+                            .font(.custom("Arial", size: 15))
+                    }
+                    
+                    ZStack {
+                        RoundedRectangle(cornerRadius: 15)
+                            .foregroundStyle(.gray)
+                            .frame(width: 100, height: 30)
+                        
+                        
+                        Text("Datum")
+                            .font(.custom("Arial", size: 15))
+                    }
+                    
+                    ZStack {
+                        RoundedRectangle(cornerRadius: 15)
+                            .foregroundStyle(.gray)
+                            .frame(width: 100, height: 30)
+                        
+                        
+                        Text("Filter")
+                            .font(.custom("Arial", size: 15))
+                    }
+                    
+                    Spacer()
+                }
+                ScrollView(.vertical, showsIndicators: false) {
+                    VStack {
+                        
+                    }
+                }
+            }
+            .padding(.top, 120)
+            .frame(maxHeight: 800)
+            
+//            top bar
+            ZStack {
+                Rectangle()
+                    .foregroundStyle(.white)
+                    .frame(width: .infinity, height: 230)
+                    .aspectRatio(contentMode: .fill)
+                    .shadow(radius: 1)
+                
+                Rectangle()
+                    .foregroundStyle(.white)
+                    .frame(width: .infinity, height: 155)
+                    .aspectRatio(contentMode: .fill)
+                    .shadow(radius: 1)
+                
+                HStack {
+                    VStack {
+                        Text("Evenemang")
+                            .bold()
+                            .font(.custom("Arial", size: 16))
+                        
+                        Rectangle()
+                            .foregroundColor(isRabatt ? .black : .white)
+                            .frame(width: 90, height: 2)
+                    }
+                    .padding(.horizontal, 50)
+                    
+                    Spacer()
+                    
+                    VStack {
+                        Text("Utforska")
+                            .bold()
+                            .font(.custom("Arial", size: 16))
+                        
+                        Rectangle()
+                            .foregroundColor(isRabatt ? .white : .black)
+                            .frame(width: 90, height: 2)
+                    }
+                    .padding(.horizontal, 50)
+                }
+                .padding(.top, 201)
+                
+                HStack {
+                    Image("bars")
+                        .resizable()
+                        .scaledToFill()
+                        .frame(width: 40, height: 40)
+                        .clipped()
+                    
+                    Spacer()
+                    
+                    Image("stuk")
+                        .resizable()
+                        .scaledToFill()
+                        .frame(width: 100, height: 40)
+                        .clipped()
+                    
+                    Spacer()
+                    
+                    Image("search")
+                        .resizable()
+                        .scaledToFill()
+                        .frame(width: 50, height: 50)
+                        .clipped()
+                }
+                .padding(.top, 105)
+                .padding(.horizontal, 10)
+            }
+            .frame(maxHeight: .infinity, alignment: .top)
+            .padding(.bottom, 820)
+//
+//            // bottom bar
+            ZStack {
+                Rectangle()
+                    .foregroundStyle(.white)
+                    .frame(width: .infinity, height: 200)
+                    .aspectRatio(contentMode: .fill)
+                    .padding(.top, 860)
+                    .shadow(radius: 1)
+                
+                HStack {
+                    Image(isHome ? "home_black" : "home_grey")
+                        .resizable()
+                        .scaledToFill()
+                        .frame(width: 40, height: 40)
+                        .clipped()
+                        .padding(14)
+                    
+                    Image(isCalender ? "calender_black" : "calender_grey")
+                        .resizable()
+                        .scaledToFill()
+                        .frame(width: 40, height: 40)
+                        .clipped()
+                        .padding(14)
+                    
+                    Image("card_grey")
+                        .resizable()
+                        .scaledToFill()
+                        .frame(width: 40, height: 40)
+                        .clipped()
+                        .padding(14)
+                    
+                    Image(isTickets ? "tickets_black" : "tickets_grey")
+                        .resizable()
+                        .scaledToFill()
+                        .frame(width: 40, height: 40)
+                        .clipped()
+                        .padding(14)
+                    
+                    Image(isProfile ? "profile_black" : "profile_grey")
+                        .resizable()
+                        .scaledToFill()
+                        .frame(width: 40, height: 40)
+                        .clipped()
+                        .padding(14)
+                }
+                .padding(.top, 720)
+            }
+            .frame(maxHeight: .infinity, alignment: .bottom)
+        }
     }
 }
 
