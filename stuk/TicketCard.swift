c96184dbaad1439482411efb62830c555260e397
axel.langenskiold.2531 <axel@langenskiold.se>
2024-09-18 22:52:28 +0200

    heheh

--- stuk/TicketCard.swift
+++ stuk/TicketCard.swift
@@ -8,13 +8,36 @@
 import SwiftUI
 
 struct TicketCard: View {
+    @State var image: String
+    
+    var ads: [String] = ["apple", "hm", "ica", "jotex", "storytel", "sats"]
     var body: some View {
         ZStack {
             
+            HStack {
+                Image(image)
+                    .resizable()
+                    .scaledToFill()
+                    .frame(width: 100, height: 100)
+                    .clipped()
+                
+                VStack(alignment: .leading) {
+                    Text(image)
+                    
+                    Text("30 dagar gratis + 50% studentrabatt")
+                    
+                    Text("Gäller till den 30 sep. 2024")
+                }
+                
+            }
+            .frame(width: 380, height: 150)
+            .shadow(radius: 5)
+            
         }
+        .frame(width: 380, height: 150)
     }
 }
 
 #Preview {
-    TicketCard()
+    TicketCard(image: "ica")
 }
