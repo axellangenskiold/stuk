fc52ece07cb6ae5289e67957c57de026e13f6f1b
axel.langenskiold.2531 <axel@langenskiold.se>
2024-09-18 22:29:23 +0200

    heheh

--- stuk/CalenderView.swift
+++ stuk/CalenderView.swift
@@ -157,9 +157,7 @@ struct CalenderView: View {
                             var transaction = Transaction()
                             transaction.disablesAnimations = true
                             withTransaction(transaction) {
-                                while path.count > 1 {
-                                    path.removeLast()
-                                }
+                                path.removeLast()
                             }
                             
                         }) {
